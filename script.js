5d1605ae6510d199e1db77021f0148ef80815cf9
Danny Borisov <dannyboris1993@gmail.com>
2020-03-24 20:27:58 +0200

    restructured backend and frontend

--- script.js
+++
@@ -1,8 +0,0 @@
-const checkboxChange=(el)=>{
-        el.parentNode.classList.toggle('checked')
-}
-
-
-fetch('https://res.cloudinary.com/dppogsm2u/image/upload/v1585049526/polar-bear-cubs_tqopog.jpg').then(res=>{
-        console.log(res)
-})
