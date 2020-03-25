780bb85ba063d1d81d5a0341bdaaf114900ed75d
Danny Borisov <dannyboris1993@gmail.com>
2020-03-25 13:57:47 +0200

    Moved client side to server public

--- server/server.js
+++ server/server.js
@@ -1,28 +1,15 @@
-const express = require('express');
-const bodyParser = require('body-parser')
 const path = require('path')
-const pug = require('pug')
-const cloudinary = require('cloudinary')
-const axios = require('axios')
+const express = require('express')
 const app = express()
+const PORT = process.env.PORT || 3000
+const bodyParser = require('body-parser')
 
-cloudinary.config({
-    cloud_name:'sample',
-    api_key:'635317355665196',
-    api_secret:'q42WtvQIkHbZ7merrtM2GT6M_Po'
-})
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname, 'public')))
+app.post('/submit',(req,res)=>{
+    console.log(req.body.name)
+    res.send(JSON.stringify(req.body))
 
-let image = cloudinary.image("polar-bear-cubs_tqopog.jpg")
-console.log(image)
-app.set('views', path.join(__dirname, 'views'))
-app.use(bodyParser.json())
-console.log('thisi s servefr')
-app.set('view engine', 'pug');
-app.use(bodyParser.urlencoded({ extended: true }));
-
-const PORT  =  process.env.PORT || 3000
-app.post('/',(req,res)=>{
-    res.render('./response.pug', {title:req.body.name});
 })
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
