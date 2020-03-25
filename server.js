37336392c9167ec1ca01aa2f6fa1001b8c6a5343
Danny Borisov <dannyboris1993@gmail.com>
2020-03-25 18:46:44 +0200

    test

--- server.js
+++ server.js
@@ -6,32 +6,32 @@ const PORT = process.env.PORT || 3000
 const bodyParser = require('body-parser')
 const User = require('./models/User')
 
-console.log(process.env)
-mongoose.connect(process.env.DB_CONNECTION ,{
-    useUnifiedTopology: true, 
-    useNewUrlParser:true}, 
-    (err)=>{try{
-        console.log('Connected')}
-    catch(err){
-        console.error(`ERROR HAS OCCURED: ${err}`)
-    }})
+try{
+    mongoose.connect(process.env.DB_CONNECTION ,{
+        useUnifiedTopology: true, 
+        useNewUrlParser:true},
+        ()=>console.log('Connected'))
+}catch{
+    console.log('Could not connect to db!!!')
+}
+
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.post('/submit', async (req,res)=>{
-    res.send('good')
-    // const user = new User({
-    //     name: req.body.name,
-    //     email:req.body.email,
-    //     age:req.body.age,
-    //     job:req.body.job,
-    //     aspects:req.body.aspects,
-    //     contribution:req.body.contribution,
-    //     comment:req.body.comment,
-    // })
-    // let newUser = await user.save()
-    // res.send(`Thank for submitting ${newUser.name}`)
+    const user = new User({
+        name: req.body.name,
+        email:req.body.email,
+        age:req.body.age,
+        job:req.body.job,
+        aspects:req.body.aspects,
+        contribution:req.body.contribution,
+        comment:req.body.comment,
+    })
+    console.log('')
+    let newUser = await user.save()
+    res.send(`Thank for submitting ${newUser.name}`)
 })
 
 
